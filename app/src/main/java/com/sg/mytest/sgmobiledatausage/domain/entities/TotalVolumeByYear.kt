f20e29df6755340a37f43730647fa801c68dbf2c
thazin <nwe.nwethazin@gmail.com>
2021-11-14 15:59:11 +0800

    chore (TotalVolumeByYear) : Fix compile error

--- app/src/main/java/com/sg/mytest/sgmobiledatausage/domain/entities/TotalVolumeByYear.kt
+++ app/src/main/java/com/sg/mytest/sgmobiledatausage/domain/entities/TotalVolumeByYear.kt
@@ -1,6 +1,10 @@
 package com.sg.mytest.sgmobiledatausage.domain.entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class TotalVolumeByYear(
     val year: Int,
     val totalVolume: Double
-)
+) : Parcelable
